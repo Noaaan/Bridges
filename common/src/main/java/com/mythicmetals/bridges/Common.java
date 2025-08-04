6461d4cd8ff7da46f9687502eb87f52135d6615e
Noaaan <noaaan@hotmail.com>
2025-08-04 22:40:40 +0200

    Fully rename to Bridges (again)

--- common/src/main/java/com/mythicmetals/bridges/Common.java
+++ common/src/main/java/com/mythicmetals/bridges/Common.java
@@ -2,7 +2,7 @@ package com.mythicmetals.bridges;
 
 public class Common {
 
-    public static final String MOD_ID = "deltarune";
+    public static final String MOD_ID = "bridges";
     public static void init() {
         System.out.println("Hello from main mod init!");
     }
